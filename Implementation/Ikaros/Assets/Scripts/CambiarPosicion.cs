1baf3ed46773711f72383eaff0b4cb96e1c60976
enriqif <enriqif@gmail.com>
2014-06-16 13:17:26 +0000

    nuevo proyecto agregado para probar repositorio

---
+++ Implementation/Ikaros/Assets/Scripts/CambiarPosicion.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarPosicion : MonoBehaviour {
+	
+	public GUITexture imagenBotonCambiarPosicion;
+	private bool botonCambiarPosicion=false;
+	private bool botonPosicionPulsado=false;
+	
+	// Use this for initialization
+	public float rotateSpeed=2.0f;
+	private bool cambiarPosicion;
+
+	void Start () {
+	this.SituarControles();
+	cambiarPosicion=true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	this.SituarControles();
+	//Para el editor
+		if(Input.GetKeyDown(KeyCode.R)){
+			if(cambiarPosicion){
+				this.cambiarOrientacionAParado();
+				cambiarPosicion=false;
+			}else{
+				this.cambiarOrientacionAAcostado();
+				cambiarPosicion=true;
+			}
+		
+		}
+	//para android
+	if(Input.touchCount > 0){
+        for (var i = 0; i < Input.touchCount; ++i) {   
+        if(imagenBotonCambiarPosicion.HitTest(Input.GetTouch(i).position)){
+					if(Input.GetTouch(i).phase==TouchPhase.Began){
+						botonPosicionPulsado = true;  
+					}
+						                   
+            }           
+        } 
+    } 
+   
+    if(botonPosicionPulsado){
+			if(cambiarPosicion){
+				this.cambiarOrientacionAParado();
+				cambiarPosicion=false;
+			}else{
+				this.cambiarOrientacionAAcostado();
+				cambiarPosicion=true;
+			}
+          
+    }	    	
+    
+    botonPosicionPulsado = false;
+	
+	}
+	private void cambiarOrientacionAParado(){
+
+		transform.localPosition=new Vector3(0,0.15f,0);
+        transform.localRotation=Quaternion.Euler(0,0,0);
+	}
+	private void cambiarOrientacionAAcostado(){
+		transform.localPosition=new Vector3(-0.1056577f,0.4757624f,-0.7931951f);
+        transform.localRotation=Quaternion.Euler(300.8452f,89.99997f,89.99997f);
+	}
+	public void SituarControles(){
+		//DERECHA		
+    	imagenBotonCambiarPosicion.transform.position = Vector3.zero;
+    	imagenBotonCambiarPosicion.transform.localScale = Vector3.zero; 
+		imagenBotonCambiarPosicion.guiTexture.pixelInset=new Rect(Screen.width*0.45f,Screen.height*0.1f,Screen.width*0.1f,Screen.height*0.06f);
+	
+		
+  }
+}
---
+++ Implementation/Ikaros/Assets/Scripts/RotarModelo.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotarModelo : MonoBehaviour {
+	//imagenes para los botones
+	public GUITexture imagenBotonIzquierda;
+	public GUITexture imagenBotonDerecha;
+	private bool botonIzquierdaPulsado=false;
+	private bool botonDerechaPulsado=false;
+	
+	public float rotateSpeed=2.0f;
+	void Awake(){
+		this.SituarControles();
+	}
+	// Use this for initialization
+	void Start () { 
+	    this.SituarControles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.SituarControles();
+		Screen.orientation=ScreenOrientation.AutoRotation;
+	//-------------------------Editor----------------------------------------
+
+	if(Input.GetKey(KeyCode.A)){
+		transform.Rotate(0, 20 * rotateSpeed * Time.deltaTime, 0);
+		}
+	if(Input.GetKey(KeyCode.D)){
+		transform.Rotate(0, -20 * rotateSpeed * Time.deltaTime, 0);	
+		}
+	//-----------------------------------------------------------------	
+		
+	 if(Input.touchCount > 0){
+        for (var i = 0; i < Input.touchCount; ++i) {   
+        if(imagenBotonIzquierda.HitTest(Input.GetTouch(i).position)){
+						botonIzquierdaPulsado = true;                     
+            }else if(imagenBotonDerecha.HitTest(Input.GetTouch(i).position)){
+						botonDerechaPulsado = true;
+            }             
+        } 
+    } 
+   
+    if(botonDerechaPulsado){
+			transform.Rotate(0, -20 * rotateSpeed * Time.deltaTime, 0);
+          
+    }else if(botonIzquierdaPulsado){
+         transform.Rotate(0, 20 * rotateSpeed * Time.deltaTime, 0); 		
+    }     
+     		    	
+    
+    botonDerechaPulsado = false;
+    botonIzquierdaPulsado = false;
+
+	}
+
+	public void SituarControles(){
+	//DERECHA
+    imagenBotonDerecha.transform.position = Vector3.zero;
+    imagenBotonDerecha.transform.localScale = Vector3.zero; 
+	imagenBotonDerecha.guiTexture.pixelInset=new Rect(Screen.width*0.8f,Screen.height*0.1f,Screen.width*0.1f,Screen.height*0.06f);
+	//IZQUIERDA
+	imagenBotonIzquierda.transform.position = Vector3.zero;
+    imagenBotonIzquierda.transform.localScale = Vector3.zero;	
+	imagenBotonIzquierda.guiTexture.pixelInset=new Rect(Screen.width*0.1f,Screen.height*0.1f,Screen.width*0.1f,Screen.height*0.06f);
+    
+  }
+
+}
